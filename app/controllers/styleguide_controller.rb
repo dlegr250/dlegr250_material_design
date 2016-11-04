02ad194628108dab11cee005f183978ff1eafa66
Daniel LeGrand <dan.legrand@gmail.com>
2016-11-04 06:57:37 -0400

    Add custom styleguide layout

--- app/controllers/styleguide_controller.rb
+++ app/controllers/styleguide_controller.rb
@@ -1,4 +1,6 @@
 class StyleguideController < ApplicationController
+  layout "styleguide"
+  
   def index
     # NOOP - page to demo interface
   end
--- lib/dlegr250_material_design/version.rb
+++ lib/dlegr250_material_design/version.rb
@@ -1,3 +1,3 @@
 module Dlegr250MaterialDesign
-  VERSION = "0.4.24"
+  VERSION = "0.4.25"
 end
