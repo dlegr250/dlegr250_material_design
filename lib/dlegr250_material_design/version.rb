beefa6e8a9c56123faa12f12c0802c933bcd9f00
Daniel LeGrand <dan.legrand@gmail.com>
2017-01-30 10:09:40 -0500

    Fix broken JS require syntax for scrollscope

--- lib/dlegr250_material_design/version.rb
+++ lib/dlegr250_material_design/version.rb
@@ -1,3 +1,3 @@
 module Dlegr250MaterialDesign
-  VERSION = "0.4.92"
+  VERSION = "0.4.93"
 end
--- vendor/assets/javascripts/dlegr250_material_design.js
+++ vendor/assets/javascripts/dlegr250_material_design.js
@@ -11,13 +11,4 @@
 //= require third_party/jquery.inputmask.min.3.2.8-11
 //= require third_party/jquery.autonumeric.1-9-44
 //= require third_party/jquery.scroll_scope.min.0.1.0
-//= require components/layout
-//= require components/menus
-//= require components/dialog
-//= require components/expansion_panels
-//= require components/tabs
-//= require components/forms
-//= require components/input_masks
-//= require components/toggles
-//= require components/snackbar
-//= require components/utility
+//= require_tree ./components
